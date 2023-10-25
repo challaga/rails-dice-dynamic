06ec5bb21e7c2f4d136797a7a11ab429d9aebb5b
challaga <146873313+challaga@users.noreply.github.com>
2023-10-25 16:37:51 +0000

    two

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -11,4 +11,16 @@ class GameController < ApplicationController
     
     render({ :template => "game_templates/twosix"})
   end
+
+  def rolled_twoten
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+  
+      @rolls.push(die)
+    end
+    
+    render({ :template => "game_templates/twoten"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+ 
   get("/dice/2/6", { :controller => "game", :action => "rolled_twosix"})
+
+  get("/dice/2/10", { :controller => "game", :action => "rolled_twoten"})
 
 end
